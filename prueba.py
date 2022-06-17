58e96bf1ea2b0793031595b4cd01409ca2beee93
Lucio Luque <lluquematerazzi@udesa.edu.ar>
2022-06-17 11:37:47 -0300

    creating a note, grapic the note, and creating a wav file with the note

--- prueba.py
+++ prueba.py
@@ -1,7 +1,27 @@
-fnbrfdnf4ed
-aasa
-xzczczc
-zcxccz
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+
+class CreateNote:
+    def __init__(self, type, amplitude, frequency, duration, starts):
+        self.type = type
+        self.amplitude = amplitude
+        self.frequency = frequency
+        self.duration = duration
+        self.starts = starts
+
+A4=CreateNote("A4", 1, 440, 1, 0)
 
 
-estoy en mi branch??
+x = np.linspace(A4.starts, A4.duration, int(44100/A4.duration))
+y = A4.amplitude * np.sin( 2*np.pi*A4.frequency * x)
+plt.plot(x, y)
+plt.grid(True)
+plt.ylabel('Amplitude')
+plt.xlabel("Time (s)")
+plt.show()
+
+
+wavfile.write("A4.wav", 44100, y)
+
+
