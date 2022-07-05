579d389482f4d3b8ff93e9c4e53287683aea3f75
Lucio Luque <lluquematerazzi@udesa.edu.ar>
2022-07-05 18:13:12 -0300

    h

--- synthesizer/prueba.py
+++ synthesizer/prueba.py
@@ -1,9 +1,13 @@
 "example of a synthesizer"
 from synthesizer import *
 from datetime import datetime
+def main():
+    instanteInicial = datetime.now()
+    s1=Synthesizer("BlackBird.txt", "flauta.txt").create_wav("BlackBird", 44100)
+    s2=Synthesizer("queen.txt", "piano.txt").create_wav("BohemianRhapsody", 44100)
+    s3=Synthesizer("Rocket-Man.txt", "piano.txt").create_wav("RocketMan", 44100)
+    instanteFinal = datetime.now()
+    print(instanteFinal - instanteInicial)
 
-instanteInicial = datetime.now()
-s=Synthesizer("BlackBird.txt", "piano.txt").create_wav("Black", 44100)
-
-instanteFinal = datetime.now()
-print(instanteFinal - instanteInicial)
+if __name__=="__main__":
+    main()
