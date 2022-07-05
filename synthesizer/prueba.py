91cfd2776857a73cfb9643a0c88aa7270c307f78
Ignacio <101135977+ignaschuemer7@users.noreply.github.com>
2022-07-05 15:55:16 -0300

    pais de la libertad added

--- synthesizer/prueba.py
+++ synthesizer/prueba.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 instanteInicial = datetime.now()
-s=Synthesizer(44100, "Estrellita_donde_estas.txt", "flauta.txt").create_wav()
+s=Synthesizer(44100, "pais_de_la_libertad.txt", "piano.txt").create_wav()
 instanteFinal = datetime.now()
 print(instanteFinal - instanteInicial)
 
